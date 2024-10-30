4ea8707ca198586662c42fd3fb3f5a98aabd6e1a
ibrakel <113463426+ibrakel@users.noreply.github.com>
2024-10-30 19:27:10 +0100

    Refactor

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import {setupCounter} from './counter.js'
-import {setupThree} from "./three.js";
+import {setupCounter} from './src/counter.js'
+import {setupThree} from "./src/three.js";
 
 document.querySelector('#app').innerHTML = `
   <div>
